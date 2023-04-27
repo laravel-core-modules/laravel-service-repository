2dcd6dc57513295740db081e2d024342516b3e2b
Rino <cocorine999@gmail.com>
2023-04-27 22:39:23 +0200

    Documentation

---
+++ tests/Feature/FeatureTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaravelCoreModule\tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class FeatureTest extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaravelCoreModule\tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitTest extends TestCase
+{
+    /**
+     * A basic test Unit.
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
